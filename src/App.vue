41baf6ea462379e1a2b828fddc52ed38397de0ce
lsshawn <coffee.or.tea88@gmail.com>
2017-08-07 11:27:13 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <div class="botui-app-container" id="my-botui-app">
+      <bot-ui></bot-ui>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import BotUI from 'botui'
+
+const botui = BotUI('my-botui-app', {
+  vue: Vue // pass the dependency.
+})
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
